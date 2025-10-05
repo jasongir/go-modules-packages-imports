b258833bcc54122e1c2a6c30e9e80d03012bd156
Jay Giroux <snujugroix@gmail.com>
2025-10-04 19:13:19 -0700

    Add doc comments

--- math.go
+++ math.go
@@ -1,5 +1,11 @@
+// Package inside test module while learning go
 package gomodulespackagesimports
 
+// Adds two integers, and returns the result
+//
+// [In-depth explanation of this novel functionality]
+//
+// [In-depth explanation of this novel functionality]: https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
